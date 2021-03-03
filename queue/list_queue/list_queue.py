c57bde70bec6f9c719b112e33688c49d17374499
molkenov <askhat.molkenov@gmail.com>
2021-03-03 17:58:24 +0600

    queue

---
+++ queue/list_queue/list_queue.py
@@ -0,0 +1,42 @@
+class List_Queue:
+    def __init__(self):
+        self.items = []
+        self.size = 0
+
+    def enqueue(self, data):
+        self.items.insert(0, data)
+        self.size += 1
+
+    def dequeue(self):
+        if self.items:
+            data = self.items.pop()
+            self.size -= 1
+            return data
+    
+    def print(self):
+        print('size:', self.size, 'items: ', end = ' ')
+        for i in self.items:
+            print (i, end=' ')
+        print('\n')
+
+
+def main():
+    queue = List_Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.print()
+    _ = queue.dequeue()
+    queue.print()
+    _ = queue.dequeue()
+    _ = queue.dequeue()
+    _ = queue.dequeue()
+    _ = queue.dequeue()
+    queue.print()    
+    queue.enqueue(4)
+    queue.enqueue(5)
+    queue.enqueue(6)    
+    queue.print()
+
+if __name__ == '__main__':
+    main()
---
+++ queue/node_queue/node_queue.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self,data=None, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+class Node_Queue:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    self.count = 0
+
+  def enqueue(self, data):
+    if (self.head == None):
+      self.head = self.tail = Node(data)
+    else:
+        node = Node(data, None, self.tail)
+        self.tail.next = node
+        self.tail = node
+    self.count += 1
+
+  def dequeue(self):
+    if self.count == 0:
+      return None
+    data = self.head.data
+    if self.count == 1:
+      self.head = None
+      self.tail = None
+    elif self.count > 1:
+      self.head = self.head.next
+      self.head.prev = None
+    self.count -= 1
+    return data
+
+  def print (self):
+    print ('count:', self.count, ', items:', end = ' ')
+    node = self.head
+    while node != None:
+        print(node.data, end = ' ')
+        node = node.next
+    print ('')
+
+def main():
+  queue = Node_Queue()
+  queue.enqueue(1)
+  queue.enqueue(2)
+  queue.enqueue(3)
+  queue.print()
+  _ = queue.dequeue()
+  queue.print()
+  _ = queue.dequeue()
+  _ = queue.dequeue()
+  _ = queue.dequeue()
+  _ = queue.dequeue()
+  queue.print()    
+  queue.enqueue(4)
+  queue.enqueue(5)
+  queue.enqueue(6)    
+  queue.print()
+
+
+if __name__ == '__main__':
+  main()
